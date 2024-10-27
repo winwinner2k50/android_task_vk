afe98af9a97cce57718d5fdc9c2e2c1bcb511ec9
Eremin Georgii <eremings@student.bmstu.ru>
2024-10-27 16:57:07 +0300

    task1'

---
+++ task1/app/src/main/java/com/example/task1/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.task1
+
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.task1.ui.theme.Task1Theme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.ui.Alignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AddContent()
+        }
+    }
+}
+
+@Composable
+private fun AddContent() {
+    val count = remember { mutableIntStateOf(0) }
+    val isPortrait = LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
+    val columns = if (isPortrait) 3 else 4
+
+    val data = List(count.intValue) { "$it" }
+
+    Column(
+        modifier = Modifier.fillMaxSize() // Используйте fillMaxSize для колонки
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(columns),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.weight(1f) // Добавьте weight для занимания оставшегося пространства
+        ) {
+            items(data) {
+                Box(
+                    modifier = Modifier
+                        .aspectRatio(1f)
+                        .background(if (it.toInt() % 2 == 0) Color.Red else Color.Blue)
+                        .padding(10.dp)
+                        .wrapContentSize(Alignment.Center)
+                ) {
+                    Text(text = "$it")
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(10.dp)
+                .wrapContentSize(Alignment.Center)
+        ) {
+            Button(
+                onClick = { count.intValue += 1 }
+            ) { Text(text = "go") }
+        }
+    }
+}
